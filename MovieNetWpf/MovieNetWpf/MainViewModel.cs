0130caaaf4a600ca1a418ec8a84ea722a6e108f3
remdev <remi.marchal6@gmail.com>
2018-02-13 11:49:12 +0100

    Debut
    Rm des WebApp
    Ajout début App WPF

---
+++ MovieNetWpf/MovieNetWpf/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace MovieNetWpf
+{
+	public class MainViewModel : ViewModelBase
+	{
+		public MainViewModel()
+		{
+			Name = "Hello MVVM";
+			MyCommand = new RelayCommand(MyCommandExecute, MycommandCanExecute);
+		}
+
+		private string _name;
+
+		public  string Name
+		{
+			get { return _name; }
+			set {
+				_name = value;
+				RaisePropertyChanged();	
+			}
+		}
+
+		public RelayCommand MyCommand { get; } 
+
+		void MyCommandExecute()
+		{
+			Name = "Hello Click";
+		}
+
+		bool MycommandCanExecute()
+		{
+			return Name != "Hello Click";
+		}
+	}
+}
